1aad66a80cbc9be34d36208b85c29db5115b0342
Halden <haldengcollier@gmail.com>
2018-05-09 23:39:16 +0100

    Basic server communication with GameMaker.

--- server.js
+++ server.js
@@ -4,9 +4,23 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 function onRequest (request, response) {
     console.log('User made a request: ' + request.url);
     response.writeHead(200, {'Context-Type': 'text/plain'});
-    response.write("Here's some data.");
+    response.write("Here's some data.\nPort: " + port.toString());
     response.end();
 }
 
-http.createServer(onRequest).listen(port);
+http.createServer(function (socket) {
+    onRequest();
+
+    socket.on('error', function(error) {
+        console.log('Socket connected.');
+    });
+
+    socket.on('end', function() {
+        console.log('Socket disconnected.');
+    });
+
+    socket.on('data', function(data) {
+        console.log('Socket data: ' + data.toString());
+    });
+}).listen(port);
 console.log('Server is running.');
