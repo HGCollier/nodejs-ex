d92effbd1a2d22fec68b055f5c77e7355daa7d51
Halden <haldengcollier@gmail.com>
2018-05-09 23:50:57 +0100

    Removed onRequest function.

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var http = require('http');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-function onRequest (request, response) {
-    console.log('User made a request: ' + request);
-    response.writeHead(200, {'Context-Type': 'text/plain'});
-    response.write("Here's some data.\nPort: " + port.toString());
-    response.end();
-}
-
 http.createServer(function (socket) {
-    onRequest();
     console.log('Socket connected.');
+
+    socket.writeHead(200, {'Context-Type': 'text/plain'});
+    socket.write("Here's some data.\nPort: " + port.toString());
+    socket.end();
 
     socket.on('error', function(error) {
         console.log('Error: ' + error.toString());
